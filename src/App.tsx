07868bf38b8e08bd72d22ec9981739abc93f9c8f
yangtian <yangtian@wimift.com>
2022-08-17 14:08:54 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import React from 'react'
+import {BrowserRouter} from 'react-router-dom'
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
